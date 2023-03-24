502a53e298df4095a7efab42346c1ad3e4170817
Rubin Won <55333006+ruby199@users.noreply.github.com>
2023-03-24 10:02:07 +0900

    191. Number of 1 Bits

---
+++ EASY/number_of_one_bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        # O(32) == O(1) constant time complexity
+        # downside: we need to count every bits even if it's not 1
+
+        res = 0
+        while n:
+            res += n % 2
+            n = n >> 1
+        return res
+
+
