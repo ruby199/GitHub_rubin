2888b887affe715c431b3957468dcc13447341cd
rwon <ruby2511004@gmail.com>
2023-04-06 22:47:38 +0900

    Update arranging_coins.py

--- EASY/arranging_coins.py
+++ EASY/arranging_coins.py
@@ -1,5 +1,8 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
+
+        # brute force solution O(n)
+        '''
         left = n
         count = 0
         i = 0
@@ -10,3 +13,20 @@ class Solution:
             count += 1
         
         return count
+        '''
+
+        # better solution O(logn)
+
+        l, r = 1, n
+        res = 0
+
+        while l <= r:
+            mid = ( l + r ) // 2
+            coins = (mid / 2) * (mid + 1) #  / means decimal division in python
+            if coins > n:
+                r = mid - 1
+            else:
+                l = mid + 1
+                res = max(mid, res)
+
+        return res
