39e386552dad717e16130f6ff09bc4bf5df31242
Rubin Won <55333006+ruby199@users.noreply.github.com>
2023-04-24 10:42:32 +0900

    22. Generate Parentheses

---
+++ Medium/generate_parenatheses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # only add open parenthesis if open > n
+        # only add a closing parenthesis if closed > open
+        # valid IIF open == closed == n
+
+        # use recursive method
+
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0) # initially stack is empty
+        return res
+
