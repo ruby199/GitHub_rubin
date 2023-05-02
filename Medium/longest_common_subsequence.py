96c7e66b93d190cc6f55e94c5f4d7c32d6bcc39f
Rubin Won <55333006+ruby199@users.noreply.github.com>
2023-05-02 09:48:58 +0900

    1143. Longest Common Subsequence

---
+++ Medium/longest_common_subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)] # initializes the 2D into 0
+
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
+                if text1[i] == text2[j]: # if they matches
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+
+        return dp[0][0] # length of the LCS of the entire text1 and text2
+    
+
+
+
+    # memory & time complexity: O(n * m) 
