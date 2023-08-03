41b31afc898b8bf6c9da6d87cbd0cb520d228546
Rubin Won <55333006+ruby199@users.noreply.github.com>
2023-08-03 09:39:14 +0900

    1498. Number of Subsequences That Satisfy the Given Sum Condition

---
+++ Medium/number_of_subsequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        # using sorting and two pointer approach
+        nums.sort()
+        res = 0
+        mod = (10**9 + 7)
+
+        r = len(nums) - 1
+        for i, left in enumerate(nums):
+            while (left + nums[r]) > target and i <= r:
+                r -= 1
+            if i <= r:
+                res += (2**(r - i))
+                res %= mod
+            
+        return res
