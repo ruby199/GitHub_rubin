3dedd3bdf3ff3ac7307b5575374387f64467c9bf
Rubin Won <55333006+ruby199@users.noreply.github.com>
2023-09-27 10:08:07 +0900

    71. Simplify Path

---
+++ Medium/simplify_path.py
@@ -0,0 +1,18 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        cur = "" # current filepath or directory we are at
+
+        for c in path + "/":
+            if c == "/":
+                if cur == "..":
+                    if stack: # if the stack is non-empty
+                        stack.pop()
+                    elif cur != "" and cur != ".":
+                        stack.append(cur) # take the current file and update our stack
+                    cur = "" # reset our current
+
+            else:
+                cur += c
+
+        return "/" + "/".join(stack)
