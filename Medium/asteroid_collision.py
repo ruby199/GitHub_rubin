2de6a2721fd31b5e91fcc9065413f7087d7e1118
Rubin Won <55333006+ruby199@users.noreply.github.com>
2023-10-17 09:40:18 +0900

    735. Asteroid Collision

---
+++ Medium/asteroid_collision.py
@@ -0,0 +1,19 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+                # have collision
+                diff = a + stack[-1]
+                if diff < 0:
+                    stack.pop()
+                elif diff > 0:
+                    a = 0
+                else:
+                    a = 0
+                    atack.pop()
+            if a:
+                stack.append(a)
+        
+        return stack
