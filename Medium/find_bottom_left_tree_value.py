0486f17556e3384ab076887d6986b5e2b1ba4249
Rubin Won <55333006+ruby199@users.noreply.github.com>
2023-11-22 11:15:43 +0900

    513. Find Bottom Left Tree Value

---
+++ Medium/find_bottom_left_tree_value.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+
+
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([root])
+
+        while q:
+            node = q.popleft()  # left-most side
+            
+            # traverse right to left (order should be right->left)
+            if node.right: q.append(node.right)
+            if node.left: q.append(node.left)
+
+        return node.val
+        
