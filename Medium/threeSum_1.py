50d1f522b31e7ce8a712ec72d2fe0af407fa30d3
Rubin Won <55333006+ruby199@users.noreply.github.com>
2024-01-25 10:09:45 +0900

    15. 3Sum

---
+++ Medium/threeSum_1.py
@@ -0,0 +1,58 @@
+"""
+[TwoSum]
+problem link: https://leetcode.com/problems/two-sum/description/
+
+[ThreeSum]
+problem link: https://leetcode.com/problems/3sum/description/
+"""
+
+def twoSum(nums, target):
+    seen = {}
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in seen:
+            return [seen[diff], i]
+        else:
+            seen[nums[i]] = i
+
+
+
+print(twoSum([2, 4, 9, 6, 5], 10))
+
+
+def threeSum(nums, target):
+    # sorts the array and use two pointer approach
+    nums.sort()
+    answer = []
+
+    for i in range(len(nums) - 2): # since we need at least 3 numbers to sum up
+        if nums[i] >0:
+            break # since we know that i is the smallest among l, r, i
+        if i > 0 and nums[i] == nums[i - 1]: # to avoid duplicates
+            continue
+        l = i + 1
+        r = len(nums) - 1
+        while l < r: # inner loop (using 2 pointers)
+            total = nums[i] + nums[l] + nums[r] # calculate the total i, l, r
+            # three possiblities
+            if total < 0:   # bc we need to make the total larger
+                l += 1
+            elif total > 0:
+                r -= 1
+            else:
+                # if we found a solution, add it to our answer list
+                triplet = [nums[i], nums[l], nums[r]]
+                answer.append(triplet)
+                
+                # check 2 extra things:
+                while l < r and nums[l] == triplet[1]:
+                    l += 1
+                while l < r and nums[r] == triplet[2]:
+                    r -= 1
+    return answer
+
+
+
+    
+
+
