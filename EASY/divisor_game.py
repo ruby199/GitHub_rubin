254c55d2b8268c3755863520761a4bb6be1117f3
Rubin Won <55333006+ruby199@users.noreply.github.com>
2024-03-21 11:44:46 +0900

    1025. Divisor Game

---
+++ EASY/divisor_game.py
@@ -0,0 +1,48 @@
+"""
+Problem Link: https://leetcode.com/problems/divisor-game/submissions/1209622630/
+
+"""
+
+
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        is_bob_turn = False
+
+        while n > 1:
+            found_move = False
+
+            for x in range(1, n):
+                if n % x == 0:
+                    n -= x
+                    found_move = True
+                    break
+            if not found_move:
+                break
+            
+            is_bob_turn = not is_bob_turn
+
+        return is_bob_turn
+    
+
+    def divisorGame_dp(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+
+        # Base case:
+        dp[0] = False
+
+        if n > 0:
+            dp[1] = False
+        
+        for i in range(2, n + 1):
+            for x in range(1, i):
+                if i % x == 0 and not dp[i - x]:
+                    dp[i] = True
+                    break
+        return dp[n]
+    
+
+# Example usage:
+sol = Solution()
+n = 4
+print(sol.divisorGame(n))  # Expected output: True
+print(sol.divisorGame_dp(n))  # Expected output: True
